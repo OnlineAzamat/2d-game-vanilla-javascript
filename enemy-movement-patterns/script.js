79c1349d9d3364ddd75db790582418454c410bba
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-08-23 22:33:58 +0500

    added enemies animation

--- enemy-movement-patterns/script.js
+++ enemy-movement-patterns/script.js
@@ -3,3 +3,46 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
+const numberOfEnemies = 10;
+const enemiesArray = [];
+
+const enemyImage = new Image();
+enemyImage.src = 'enemy1.png';
+
+class Enemy {
+  constructor() {
+    this.x = Math.random() * canvas.width;
+    this.y = Math.random() * canvas.height;
+    this.speed = Math.random() * 4 - 2;
+    this.spriteWidth = 293;
+    this.spriteHeight = 155;
+    this.width = this.spriteWidth / 2.5;
+    this.height = this.spriteHeight / 2.5;
+    this.frame = 0;
+  }
+  update() {
+    this.x += this.speed;
+    this.y += this.speed;
+    // animate sprites
+    this.frame > 4 ? this.frame = 0 : this.frame++;
+  }
+  draw() {
+    ctx.strokeRect(this.x, this.y, this.width, this.height);
+    ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+  }
+}
+
+for (let i = 0; i < numberOfEnemies; i++) {
+  enemiesArray.push(new Enemy());
+}
+
+function animate() {
+
+  ctx.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
+  enemiesArray.forEach(enemy => {
+    enemy.update();
+    enemy.draw();
+  })
+  requestAnimationFrame(animate);
+}
+animate();
