b42920fcb50e0e393fc6a3bbca576b5568b7f7e3
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-08-25 20:07:08 +0500

    logic updated

--- enemy-movement-patterns/script.js
+++ enemy-movement-patterns/script.js
@@ -3,32 +3,39 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 10;
+const numberOfEnemies = 15;
 const enemiesArray = [];
 
-const enemyImage = new Image();
-enemyImage.src = 'enemy1.png';
+let gameFrame = 0;
 
 class Enemy {
   constructor() {
-    this.x = Math.random() * canvas.width;
-    this.y = Math.random() * canvas.height;
-    this.speed = Math.random() * 4 - 2;
-    this.spriteWidth = 293;
-    this.spriteHeight = 155;
+    this.image = new Image();
+    this.image.src = 'enemy2.png';
+    this.speed = Math.random() * 4 + 1;
+    this.spriteWidth = 266;
+    this.spriteHeight = 188;
     this.width = this.spriteWidth / 2.5;
     this.height = this.spriteHeight / 2.5;
+    this.x = Math.random() * (canvas.width - this.width);
+    this.y = Math.random() * (canvas.height - this.height);
     this.frame = 0;
+    this.flapSpeed = Math.floor(Math.random() * 3 + 1);
+    this.angle = Math.random() * 2;
+    this.angleSpeed = Math.random() * 0.2;
   }
   update() {
-    this.x += this.speed;
-    this.y += this.speed;
+    this.x -= this.speed;
+    this.y += Math.sin(this.angle);
+    this.angle += 0.05;
+    if (this.x + this.width < 0) this.x = canvas.width;
     // animate sprites
-    this.frame > 4 ? this.frame = 0 : this.frame++;
+    if(gameFrame % this.flapSpeed === 0) {
+      this.frame > 4 ? this.frame = 0 : this.frame++;
+    } 
   }
   draw() {
-    ctx.strokeRect(this.x, this.y, this.width, this.height);
-    ctx.drawImage(enemyImage, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
+    ctx.drawImage(this.image, this.frame * this.spriteWidth, 0, this.spriteWidth, this.spriteHeight, this.x, this.y, this.width, this.height);
   }
 }
 
@@ -43,6 +50,7 @@ function animate() {
     enemy.update();
     enemy.draw();
   })
+  gameFrame++;
   requestAnimationFrame(animate);
 }
 animate();
