a5f87d06444a6cc1cbaabdc0a7a352739586fdf4
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-09-29 22:02:15 +0500

    enemy3 completed

--- enemy-movement-patterns/script.js
+++ enemy-movement-patterns/script.js
@@ -3,7 +3,7 @@ const canvas = document.getElementById('canvas1');
 const ctx = canvas.getContext('2d');
 CANVAS_WIDTH = canvas.width = 500;
 CANVAS_HEIGHT = canvas.height = 1000;
-const numberOfEnemies = 20;
+const numberOfEnemies = 25;
 const enemiesArray = [];
 
 let gameFrame = 0;
@@ -19,16 +19,21 @@ class Enemy {
     this.height = this.spriteHeight / 2.5;
     this.x = Math.random() * (canvas.width - this.width);
     this.y = Math.random() * (canvas.height - this.height);
+    this.newX = Math.random() * (canvas.width - this.width);
+    this.newY = Math.random() * (canvas.height - this.height);
     this.frame = 0;
     this.flapSpeed = Math.floor(Math.random() * 3 + 1);
-    this.angle = 0;
-    this.angleSpeed = Math.random() * 1.5 + 0.5;
-    this.curve = Math.random() * 200 + 50;
+    this.interval = Math.floor(Math.random() * 200 + 50);
   }
   update() {
-    this.x = this.curve * Math.sin(this.angle * Math.PI/90) + (canvas.width/2 - this.width/2);
-    this.y = this.curve * Math.cos(this.angle * Math.PI/360) + (canvas.height/2 - this.height/2);
-    this.angle += this.angleSpeed;
+    if(gameFrame % this.interval === 0) {
+      this.newX = Math.random() * (canvas.width - this.width);
+      this.newY = Math.random() * (canvas.height - this.height);
+    }
+    let dx = this.x - this.newX;
+    let dy = this.y - this.newY;
+    this.x -= dx/70;
+    this.y -= dy/70;
     if (this.x + this.width < 0) this.x = canvas.width;
     // animate sprites
     if(gameFrame % this.flapSpeed === 0) {
