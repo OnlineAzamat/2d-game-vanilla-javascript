e06e6c261b0eb14dc00af290648abc657b817ba0
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-12-25 16:43:39 +0500

    feat: Added score and game over logic to side-scroller game

--- side-scroller/script.js
+++ side-scroller/script.js
@@ -4,6 +4,8 @@ window.addEventListener('load', function() {
   canvas.width = 800;
   canvas.height = 720;
   let enemies = [];
+  let score = 0;
+  let gameOver = false;
 
   class InputHandler {
     constructor() {
@@ -39,20 +41,40 @@ window.addEventListener('load', function() {
       this.image = document.getElementById('playerImage');
       this.frameX = 0;
       this.frameY = 0;
+      this.fps = 20;
+      this.frameTimer = 0;
+      this.frameInterval = 1000/this.fps;
       this.maxFrame = 8;
       this.speed = 0;
       this.vy = 0;
       this.weight = 1;
     }
     draw(context) {
-      // context.fillStyle = 'white';
-      // context.fillRect(this.x, this.y, this.width, this.height);
+      context.strokeStyle = 'white';
+      context.strokeRect(this.x, this.y, this.width, this.height);
+      context.beginPath();
+      context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+      context.stroke();
+      context.strokeStyle = 'blue';
+      context.beginPath();
+      context.arc(this.x, this.y, this.width/2, 0, Math.PI * 2);
+      context.stroke();
       context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
     }
-    update(input) {
-      if (this.frameX >= this.maxFrame) {
-
-      }
+    update(input, deltaTime, enemies) {
+      // collision detection
+      enemies.forEach(enemy => {
+        const dx = (enemy.x + enemy.width/2) - (this.x + this.width/2);
+        const dy = (enemy.y + enemy.height/2) - (this.y + this.height/2);
+        const distance = Math.sqrt(dx * dx + dy * dy);
+        if (distance < enemy.width/2 + this.width/2) {
+          gameOver = true;
+        }
+      });
+      // sprite animation
+      if (this.frameX >= this.maxFrame) this.frameX = 0;
+      else this.frameX++;
+      // controls
       if (input.keys.indexOf('ArrowRight') > -1) {
         this.speed = 5;
       } else if(input.keys.indexOf('ArrowLeft') > -1) {
@@ -70,9 +92,11 @@ window.addEventListener('load', function() {
       this.y += this.vy;
       if (!this.onGround()) {
         this.vy += this.weight;
+        this.maxFrame = 5;
         this.frameY = 1;
       } else {
         this.vy = 0;
+        this.maxFrame = 8;
         this.frameY = 0;
       }
       if (this.y > this.gameHeight - this.height) this.y = this.gameHeight - this.height
@@ -118,9 +142,20 @@ window.addEventListener('load', function() {
       this.frameTimer = 0;
       this.frameInterval = 1000/this.fps;
       this.speed = 8;
+      this.markedForDeletion = false;
     }
     draw(context) {
-      context.drawImage(this.image, 0 * this.width, 0 * this.height, this.width, this.height, this.x, this.y, this.width, this.height); //5:10:57
+      context.strokeStyle = 'white';
+      context.strokeRect(this.x, this.y, this.width, this.height);
+      context.beginPath();
+      context.arc(this.x + this.width/2, this.y + this.height/2, this.width/2, 0, Math.PI * 2);
+      context.stroke();
+      context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height); //5:10:57
+      context.strokeStyle = 'blue';
+      context.beginPath();
+      context.arc(this.x, this.y, this.width/2, 0, Math.PI * 2);
+      context.stroke();
+      context.drawImage(this.image, this.frameX * this.width, 0, this.width, this.height, this.x, this.y, this.width, this.height); //5:10:57
     }
     update(deltaTime) {
       if (this.frameTimer > this.frameInterval) {
@@ -131,12 +166,17 @@ window.addEventListener('load', function() {
         this.frameTimer += deltaTime;
       }
       this.x -= this.speed;
+      if (this.x < 0 - this.width) {
+        this.markedForDeletion = true;
+        score++;
+      }
     }
   }
 
   function handleEnemies(deltaTime) {
     if (enemyTimer > enemyInterval + randomEnemyInterval) {
       enemies.push(new Enemy(canvas.width, canvas.height));
+      console.log(enemies);
       randomEnemyInterval = Math.random() * 1000 + 500;
       enemyTimer = 0;
     } else {
@@ -145,11 +185,23 @@ window.addEventListener('load', function() {
     enemies.forEach(enemy => {
       enemy.draw(ctx);
       enemy.update(deltaTime);
-    })
+    });
+    enemies = enemies.filter(enemy => !enemy.markedForDeletion);
   }
 
-  function displayStatusText() {
-
+  function displayStatusText(context) {
+    context.font = '40px Helvetica';
+    context.fillStyle = 'black';
+    context.fillText('Score: ' + score, 20, 50);
+    context.fillStyle = 'white';
+    context.fillText('Score: ' + score, 22, 52);
+    if (gameOver) {
+      context.textAlign = 'center';
+      context.fillStyle = 'black';
+      context.fillText('GAME OVER, try again!', canvas.width/2, 200);
+      context.fillStyle = 'white';
+      context.fillText('GAME OVER, try again!', canvas.width/2 + 2, 202);
+    }
   }
 
   const input = new InputHandler();
@@ -168,9 +220,10 @@ window.addEventListener('load', function() {
     background.draw(ctx);
     // background.update();
     player.draw(ctx);
-    player.update(input);
+    player.update(input, deltaTime, enemies);
     handleEnemies(deltaTime);
-    requestAnimationFrame(animate);
+    displayStatusText(ctx);
+    if (!gameOver) requestAnimationFrame(animate);
   }
   animate(0);
 });
