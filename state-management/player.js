f53149ebd6e06e418c43dfd39fdd3cf5519e068e
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-12-28 13:13:51 +0500

    feat: Added state management and input handling features

--- state-management/player.js
+++ state-management/player.js
@@ -7,12 +7,16 @@ export default class Player {
     this.image = document.getElementById('dogImage');
     this.width = 200;
     this.height = 181.83;
-    this.x = 0;
-    this.y = 0;
+    this.x = this.gameWidth/2 - this.width/2;
+    this.y = this.gameHeight - this.height;
     this.frameX = 0;
-    this.frameY = 3;
+    this.frameY = 0;
   }
   draw(context) {
     context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+  }
+  setState(state) {
+    this.currentState = this.states[state];
+    this.currentState.enter();
   }
 }
---
+++ state-management/state.js
@@ -0,0 +1,35 @@
+export const states = {
+  STANDING_LEFT: 0,
+  STANDING_RIGHT: 1,
+}
+
+class State {
+  constructor(state) {
+    this.state = state;
+  }
+}
+
+class StandingLeft extends State {
+  constructor(player) {
+    super('STANDING LEFT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 0;
+  }
+  handleInput(input) {
+    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT); // 6:28:11
+  }
+}
+class StandingRight extends State {
+  constructor(player) {
+    super('STANDING RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 1;
+  }
+  handleInput(input) {
+    if (input === 'PRESS left') // set state to StandingLeft
+  }
+}
