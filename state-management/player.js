d81b6267c0b22cfc8293ba03e58ae6f4000a8a51
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-12-28 10:05:39 +0500

    feat: Added Player class to state-management

---
+++ state-management/player.js
@@ -0,0 +1,18 @@
+export default class Player {
+  constructor(gameWidth, gameHeight) {
+    this.gameWidth = gameWidth;
+    this.gameHeight = gameHeight;
+    this.states = [];
+    this.currentState = this.states[0];
+    this.image = document.getElementById('dogImage');
+    this.width = 200;
+    this.height = 181.83;
+    this.x = 0;
+    this.y = 0;
+    this.frameX = 0;
+    this.frameY = 3;
+  }
+  draw(context) {
+    context.drawImage(this.image, this.width * this.frameX, this.height * this.frameY, this.width, this.height, this.x, this.y, this.width, this.height);
+  }
+}
