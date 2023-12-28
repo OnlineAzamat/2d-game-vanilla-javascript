d37755d5fa1c900de16f37700e5c301d15512f95
OnlineAzamat <yakubbaev.azamat@mail.ru>
2023-12-28 16:36:42 +0500

    feat: Added ArrowDown, ArrowUp, and associated state changes for player movements

--- state-management/state.js
+++ state-management/state.js
@@ -1,6 +1,8 @@
 export const states = {
   STANDING_LEFT: 0,
   STANDING_RIGHT: 1,
+  SITTING_LEFT: 2,
+  SITTING_RIGHT: 3,
 }
 
 class State {
@@ -9,27 +11,55 @@ class State {
   }
 }
 
-class StandingLeft extends State {
+export class StandingLeft extends State {
   constructor(player) {
     super('STANDING LEFT');
-    this.player = player;
-  }
-  enter() {
-    this.player.frameY = 0;
-  }
-  handleInput(input) {
-    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT); // 6:28:11
-  }
-}
-class StandingRight extends State {
-  constructor(player) {
-    super('STANDING RIGHT');
     this.player = player;
   }
   enter() {
     this.player.frameY = 1;
   }
   handleInput(input) {
-    if (input === 'PRESS left') // set state to StandingLeft
+    if (input === 'PRESS right') this.player.setState(states.STANDING_RIGHT); // 6:28:11
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_LEFT); // 6:42:19
+  }
+}
+export class StandingRight extends State {
+  constructor(player) {
+    super('STANDING RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 0;
+  }
+  handleInput(input) {
+    if (input === 'PRESS left') this.player.setState(states.STANDING_LEFT);
+    else if (input === 'PRESS down') this.player.setState(states.SITTING_RIGHT);
+  }
+}
+export class SittingLeft extends State {
+  constructor(player) {
+    super('SITTING LEFT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 8;
+  }
+  handleInput(input) {
+    if (input === 'PRESS right') this.player.setState(states.SITTING_RIGHT);
+    else if (input === 'PRESS up') this.player.setState(states.STANDING_LEFT);
+  }
+}
+export class SittingRight extends State {
+  constructor(player) {
+    super('SITTING RIGHT');
+    this.player = player;
+  }
+  enter() {
+    this.player.frameY = 8;
+  }
+  handleInput(input) {
+    if (input === 'PRESS left') this.player.setState(states.SITTING_LEFT);
+    else if (input === 'PRESS up') this.player.setState(states.STANDING_RIGHT);
   }
 }
