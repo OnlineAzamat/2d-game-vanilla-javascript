43cbe4c613cfccc00bfb80f3d3a870830d952528
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-01-02 23:08:55 +0500

    feat: Added InputHandler class with keydown event listener

--- final/input.js
+++ final/input.js
@@ -1,3 +1,11 @@
-class Input {
-  
+export class InputHandler {
+  constructor() {
+    this.keys = [];
+    window.addEventListener('keydown', e => {
+      console.log(e.key);
+      if (e.key === 'ArrowDown' && this.keys.indexOf(e.key) === -1) {
+        this.keys.push(e.key);
+      }
+    });
+  }
 }
