f23f8274f2eaef3984dd25d692512a4585a28836
OnlineAzamat <yakubbaev.azamat@mail.ru>
2024-01-05 22:15:58 +0500

    feat: Added Falling, Jumping, and Running states to player.js

--- final/player.js
+++ final/player.js
@@ -1,4 +1,7 @@
 import { 
+  Falling,
+  Jumping,
+  Running,
   Sitting,
 } from "./playerStates.js";
 
@@ -14,15 +17,20 @@ export class Player {
     this.image = document.getElementById('player');
     this.frameX = 0;
     this.frameY = 0;
+    this.maxFrame = 5;
     this.speed = 0;
     this.maxSpeed = 10;
     this.states = [
       new Sitting(this),
+      new Running(this),
+      new Jumping(this),
+      new Falling(this),
     ];
     this.currentState = this.states[0];
     this.currentState.enter();
   }
   update(input) {
+    this.currentState.handleInput(input);
     // horizantal movement
     this.x += this.speed;
     if (input.includes('ArrowRight')) this.speed = this.maxSpeed;
@@ -31,12 +39,12 @@ export class Player {
     if (this.x < 0) this.x = 0;
     if (this.x > this.game.width - this.width) this.x = this.game.width - this.width;
     // vertical movement
-    if (input.includes('ArrowUp') && this.onGround()) {
-      this.vy -= 30;
-    }
     this.y += this.vy;
     if (!this.onGround()) this.vy += this.weight;
     else this.vy = 0;
+    // sprite animation
+    if (this.frameX < this.maxFrame) this.frameX++;
+    else this.frameX = 0; // 7:49:00
   }
   draw(context) {
     context.drawImage(this.image, this.frameX * this.width, this.frameY * this.height, this.width, this.height, this.x, this.y, this.width, this.height);
